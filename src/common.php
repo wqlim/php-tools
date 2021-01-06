6b4e3c329ac51be3c4d32978bf837af033cf5da7
wqlim <wqlim@qq.com>
2021-01-07 00:38:35 +0800

    add functions

--- src/common.php
+++ src/common.php
@@ -69,6 +69,20 @@ if (!function_exists('check_mobile')) {
     }
 }
 
+if (!function_exists('check_email')) {
+    /**
+     * 邮箱码验证
+     * @param $email 邮箱
+     */
+    function check_email($email)
+    {
+        if (!is_numeric($email)) {
+            return false;
+        }
+        return preg_match('#^13[\d]{9}$|^14[5,7]{1}\d{8}$|^15[^4]{1}\d{8}$|^17[0,3,6,7,8]{1}\d{8}$|^18[\d]{9}$#', $email) ? true : false;
+    }
+}
+
 if (!function_exists('send_http_request')) {
     /**
      * 发送HTTP请求
